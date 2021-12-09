f29d6ed3aaa2ab666e83f141cf1cf065f3ce3061
Hoang Le <hoangle@MacBook-Pro.local>
2021-12-09 09:40:33 +0700

    add e2e test for Auth

--- src/middlewares/current-user.middleware.ts
+++ src/middlewares/current-user.middleware.ts
@@ -1,7 +1,7 @@
 import { Injectable, NestMiddleware } from '@nestjs/common';
 import { NextFunction, Request, Response } from 'express';
-import { User } from 'src/users/user.entity';
-import { UsersService } from 'src/users/users.service';
+import { User } from '../users/user.entity';
+import { UsersService } from '../users/users.service';
 
 declare global {
   namespace Express {
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,7 @@
 import { Module, MiddlewareConsumer } from '@nestjs/common';
 // import { APP_INTERCEPTOR } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { CurrentUserMiddleware } from 'src/middlewares/current-user.middleware';
+import { CurrentUserMiddleware } from '../middlewares/current-user.middleware';
 import { AuthService } from './auth.service';
 // import { CurrentUserInterceptor } from './interceptors/current-user.interceptor';
 import { User } from './user.entity';
